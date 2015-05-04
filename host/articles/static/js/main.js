12bf5462faa66610e3acefdbb047abf6ddb7e968
Chris Barna <chris@unbrain.net>
2015-05-04 16:21:54 -0400

    "Mobile-first"?

--- host/articles/static/js/main.js
+++ host/articles/static/js/main.js
@@ -1,2 +1,7 @@
 (function() {
+    $(".annotation-link").on("click", function(e) {
+        e.preventDefault();
+        var annotation_id = $(this).data("annotation");
+        $(".annotation[data-annotation=" + annotation_id + "]").toggleClass("active");
+    });
 })();
