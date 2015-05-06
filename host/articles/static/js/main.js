1e22f61ba4be68cabf5b627473341990a1d0521b
Chris Barna <chris@unbrain.net>
2015-05-06 11:13:14 -0400

    Fix up annotations for print styles.

--- host/articles/static/js/main.js
+++ host/articles/static/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(".article-body .annotation-link").on("click", function(e) {
         e.preventDefault();
         var annotation_id = $(this).data("annotation");
-        $(this).toggleClass("active");
+        $(".article-body .annotation-link[data-annotation=" + annotation_id + "]").toggleClass("active");
         $(".article-body .annotation[data-annotation=" + annotation_id + "]").toggleClass("active").attr("style", "");
     });
 });
